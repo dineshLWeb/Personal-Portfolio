7b93553d4511eb48bc157466af7b596678396d0b
Dinesh lokhande <dineshlokhande702@gmail.com>
2024-10-12 13:40:31 +0530

    Initial commit

---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "Webpage design",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "02",
+    s_name: "website design",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "03",
+    s_name: "Social media",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "04",
+    s_name: "App design",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "05",
+    s_name: "Frappe",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "06",
+    s_name: "Dashboard",
+    s_desc: "Web development is the process of building, programming...",
+  },
+];
+
+export default Services_Data;
